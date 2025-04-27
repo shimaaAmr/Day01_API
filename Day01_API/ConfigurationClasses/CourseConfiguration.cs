25a309268cb5f3f0be5adc7ed59b6a767d0f81bd
Online <Abstract Programmer>
2025-04-27 01:57:33 -1000

    Add data access

---
+++ Day01_API/ConfigurationClasses/CourseConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Day01_API.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Day01_API.Configure
+{
+	public class CourseConfiguration : IEntityTypeConfiguration<Course>
+	{
+		void IEntityTypeConfiguration<Course>.Configure(EntityTypeBuilder<Course> builder)
+		{
+			builder.HasKey(c => c.Id);
+
+			builder.Property(c => c.Name)
+				.HasMaxLength(50)
+				.IsUnicode(true)
+				.IsRequired(false);
+
+			builder.Property(c => c.Description)
+				.HasMaxLength(150)
+				.IsUnicode(true)
+				.IsRequired(false);
+
+			builder.Property(c => c.Duration)
+				.IsRequired(false);
+
+		}
+	}
+}
---
+++ Day01_API/Models/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Day01_API.Configure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Day01_API.Models
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext() : base()
+		{
+
+		}
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+
+		}
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.ApplyConfiguration(new CourseConfiguration());
+		}
+		public DbSet<Course> Courses { get; set; }
+	}
+}
